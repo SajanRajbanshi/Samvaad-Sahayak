422507b8476f68e7796185359f5e92d682372c95
Satyanarayan Sahoo <forsatyanarayansahoo@gmail.com>
2023-09-15 16:24:23 +0530

    Added Dependencies and script.js With all functions

---
+++ script.js
@@ -0,0 +1,40 @@
+import { createWorker } from 'tesseract.js';
+const fs = require('fs');
+const pdf = require('pdf-parse');
+
+async function recognizeText(imgPath) {
+
+    const worker = await createWorker({
+        logger: m => console.log(m)
+    });
+
+
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    const { data: { text } } = await worker.recognize(imgPath);
+    await worker.terminate();
+    console.log(text);
+    return text;
+}
+
+async function translateText(text, targetLang) {
+    const encodeText=encodeURIComponent(text);
+    const response = await fetch("https://lingva.garudalinux.org/api/v1/auto/" + targetLang + "/" + encodeText)
+    const obj = await response.json();
+    console.log(obj);
+    return obj['translation']
+}
+
+async function extractTextFromPDF(pdfPath) {
+    const dataBuffer = fs.readFileSync(pdfPath);
+    const data = await pdf(dataBuffer);
+  
+    // Extracted text is available in the `text` property of the data object
+    const text = data.text;
+  
+    // Clean up the text by removing extra whitespace
+    const cleanedText = text.replace(/\s+/g, ' ').trim();
+  
+    return cleanedText;
+  }
+  
