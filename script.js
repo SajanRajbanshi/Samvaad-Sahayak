44f3c51fc1daf553ad061d10751eb9f101b58eeb
parthiv11 <parthivmakwana11@gmail.com>
2023-09-16 02:04:32 +0530

    Doc to Text Working

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-// import { createWorker } from "tesseract.js";
-// import pdf from 'pdf-parse';
-// import fs  from 'fs';
-// export async function recognizeText(imgPath) {
-//     const worker = await createWorker({
-//         logger: m => console.log(m)
-//     });
-
-
-//     await worker.loadLanguage('eng');
-//     await worker.initialize('eng');
-//     const { data: { text } } = await worker.recognize(imgPath);
-//     await worker.terminate();
-//     console.log(text);
-//     return text;
-// }
 
 export async function translateText(text, targetLang) {
     const encodeText=encodeURIComponent(text);
@@ -22,17 +6,3 @@ export async function translateText(text, targetLang) {
     console.log(obj);
     return obj['translation']
 }
-
-export async function extractTextFromPDF(pdfPath) {
-    const dataBuffer = fs.readFileSync(pdfPath);
-    const data = await pdf(dataBuffer);
-  
-    // Extracted text is available in the `text` property of the data object
-    const text = data.text;
-  
-    // Clean up the text by removing extra whitespace
-    const cleanedText = text.replace(/\s+/g, ' ').trim();
-  
-    return cleanedText;
-  }
-  
